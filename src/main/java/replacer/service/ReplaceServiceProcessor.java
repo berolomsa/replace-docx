5baf0ce24f74776c3085921a140591fabbeab512
berolomsa <bloms12@freeuni.edu.ge>
2019-03-23 11:52:32 +0100

    From Now replacing also $\{\} variables

--- src/main/java/replacer/service/ReplaceServiceProcessor.java
+++ src/main/java/replacer/service/ReplaceServiceProcessor.java
@@ -2,10 +2,9 @@ package replacer.service;
 
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.POIXMLException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
-import org.apache.poi.xwpf.usermodel.XWPFParagraph;
-import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.apache.poi.xwpf.usermodel.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +19,7 @@ import replacer.model.Response;
 import javax.servlet.http.HttpServletRequest;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -27,6 +27,7 @@ import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 import static replacer.utils.Constants.*;
@@ -45,7 +46,7 @@ public class ReplaceServiceProcessor {
         logger.info("Generated filename : {}", generatedFileName);
 
         try {
-            replace(normalizeMap(request.getReplace()),  createFilePath(request.getFile(), DIR + DIR_TEMPLATES), createFilePath(generatedFileName, DIR + DIR_TEMPS));
+            replace2(normalizeMap(request.getReplace()),  createFilePath(request.getFile(), DIR + DIR_TEMPLATES), createFilePath(generatedFileName, DIR + DIR_TEMPS));
             logger.info("Created file : {}", generatedFileName);
             response.setUrl(createURL(generatedFileName, httpServletRequest));
             response.setStatus(HttpStatus.OK.name());
@@ -93,22 +94,83 @@ public class ReplaceServiceProcessor {
                 .collect(Collectors.toList());
     }
 
-    private static void replace(Map<String, String> map, String file, String out) throws Exception {
-        XWPFDocument doc = new XWPFDocument(OPCPackage.open(file));
-        for (XWPFParagraph p : doc.getParagraphs()) {
-            List<XWPFRun> runs = p.getRuns();
-            if (!CollectionUtils.isEmpty(runs)) {
-                for (XWPFRun r : runs) {
-                    final String text = r.getText(0);
-                    if (StringUtils.isEmpty(text)) continue;
-                    map.entrySet()
-                            .stream()
-                            .filter(a -> text.contains(convertToKey(a.getKey())))
-                            .forEach(a -> r.setText(r.getText(0).replace(convertToKey(a.getKey()), a.getValue()), 0));
-                }
-            }
-        }
-        doc.write(new FileOutputStream(out));
-    }
+    //$KEY$
+	private void replaceOld(Map<String, String> map, XWPFDocument doc) throws Exception {
+		for (XWPFParagraph p : doc.getParagraphs()) {
+			List<XWPFRun> runs = p.getRuns();
+			if (!CollectionUtils.isEmpty(runs)) {
+				for (XWPFRun r : runs) {
+					final String text = r.getText(0);
+					if (StringUtils.isEmpty(text)) continue;
+					map.entrySet()
+							.stream()
+							.filter(a -> text.contains(convertToKey(a.getKey())))
+							.forEach(a -> r.setText(r.getText(0).replace(convertToKey(a.getKey()), a.getValue()), 0));
+				}
+			}
+		}
+	}
+
+    private void replace2(Map<String, String> map, String file, String out) throws Exception{
+    	XWPFDocument xwpfDocument = openDocument(file);
+		replaceOld(map, xwpfDocument);
+		replaceNew(map, xwpfDocument);
+		writeAndClose(out,  xwpfDocument);
+	}
+
+	private static XWPFDocument openDocument(String filePath) throws Exception {
+		return new XWPFDocument(OPCPackage.open(filePath));
+	}
+
+	private void writeAndClose(String fileResult, XWPFDocument document) throws IOException {
+		document.write(new FileOutputStream(fileResult));
+	}
+
+	private void iterateParagraphs(XWPFDocument doc, Consumer<XWPFParagraph> consumer) {
+		for (XWPFParagraph p : doc.getParagraphs())
+			consumer.accept(p);
+		for (XWPFTable tbl : doc.getTables())
+			for (XWPFTableRow row : tbl.getRows())
+				for (XWPFTableCell cell : row.getTableCells())
+					for (XWPFParagraph p : cell.getParagraphs())
+						consumer.accept(p);
+	}
+
+	//${KEY}
+	private void replaceNew(Map<String, String> fieldsForReport, XWPFDocument document) {
+		iterateParagraphs(document, p -> replaceParagraph(p, fieldsForReport));
+	}
+
+	private void replaceParagraph(XWPFParagraph paragraph, Map<String, String> fieldsForReport) throws POIXMLException {
+		String find, text, runsText;
+		List<XWPFRun> runs;
+		XWPFRun run, nextRun;
+		for (String key : fieldsForReport.keySet()) {
+			text = paragraph.getText();
+			if (!text.contains("${"))
+				return;
+			find = "${" + key + "}";
+			if (!text.contains(find))
+				continue;
+			runs = paragraph.getRuns();
+			for (int i = 0; i < runs.size(); i++) {
+				run = runs.get(i);
+				runsText = run.getText(0);
+				if (runsText != null) {
+					if (runsText.contains("${") ||
+							(runsText.contains("$") && runs.size() > 1 && runs.get(i + 1).getText(0).substring(0, 1).equals("{"))) {
+						while (!runsText.contains("}")) {
+							nextRun = runs.get(i + 1);
+							runsText = runsText + nextRun.getText(0);
+							paragraph.removeRun(i + 1);
+						}
+						run.setText(runsText.contains(find) ?
+								runsText.replace(find, fieldsForReport.get(key)) :
+								runsText, 0);
+					}
+				}
+			}
+		}
+	}
 
 }
